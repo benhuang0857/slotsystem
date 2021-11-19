06ef18bfa26fd5adb406f399fed1990ea02b05e2
benhuang0857 <benhuang0857@gmail.com>
2021-11-20 02:18:19 +0800

    20211120

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+
+<style>
+    body, html {
+    height: 100%;
+    }
+
+    .bg {
+    /* The image used */
+    background-image: linear-gradient(
+      rgba(0, 0, 0, 0.7), 
+      rgba(0, 0, 0, 0.7)
+    ),url("https://previews.123rf.com/images/leksustuss/leksustuss1912/leksustuss191200034/134819842-golden-slot-machine-wins-the-jackpot-777-on-the-background-of-an-explosion-of-coins-vector-illustrat.jpg");
+
+    /* Full height */
+    height: 100%;
+
+    /* Center and scale the image nicely */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    
+    }
+</style>
+
+<div class="container bg">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
