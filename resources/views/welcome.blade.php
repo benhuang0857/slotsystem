57ea0cee137730c0d5e416b38f2102f2faca24f0
benhuang0857 <benhuang0857@gmail.com>
2021-12-07 22:49:02 +0800

    20211207

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,8 +33,24 @@
     
     }
 </style>
-    </head>
+    </head>    
     <body>
+
+        <div class="container">
+            <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+                <a href="/" class="col-12 align-items-center text-dark text-decoration-none" style="text-align: center;">
+                    <span style="font-size:30px">天鷹</span>
+                </a>
+            
+                <ul class="nav col-12 justify-content-center mb-md-0" style="font-size: 25px">
+                    <li><a href="#" class="nav-link px-2 link-secondary">首頁</a></li>
+                    <li><a href="#" class="nav-link px-2 link-dark">斯洛</a></li>
+                    <li><a href="#" class="nav-link px-2 link-dark">魚機</a></li>
+                    <li><a href="#" class="nav-link px-2 link-dark">鋼珠</a></li>
+                    <li><a href="#" class="nav-link px-2 link-dark">客戶</a></li>
+                </ul>
+            </header>
+        </div>
 
         <div class="album py-5" style="background: black;">
             <div class="container">
@@ -61,8 +77,8 @@
                                 <p class="card-text">{{$machine->name}}</p>
                                 <div class="d-flex justify-content-between align-items-center">
                                     <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">進入</button>
+                                    
                                     </div>
                                 </div>
                                 </div>
@@ -75,5 +91,46 @@
         </div>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
+        <div class="container">
+            <footer class="row row-cols-5 py-5 my-5 border-top">
+              <div class="col">
+                <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+                    天鷹
+                </a>
+                <p class="text-muted">© 2021</p>
+              </div>
+          
+              <div class="col">
+          
+              </div>
+          
+              <div class="col">
+                <h5>首頁</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">儲值</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">會員專區</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">優惠</a></li>
+                </ul>
+              </div>
+          
+              <div class="col">
+                <h5>遊戲</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">斯洛</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">鋼珠</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">魚機</a></li>
+                </ul>
+              </div>
+          
+              <div class="col">
+                <h5>QA</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">如何儲值</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">關於我們</a></li>
+                </ul>
+              </div>
+            </footer>
+        </div>
     </body>
 </html>
